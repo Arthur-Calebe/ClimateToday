de4d94266a26f79687bbd069120c645aac24293b
Arthur-Calebe <arthurcarvalhodas@gmail.com>
2026-05-24 16:46:54 -0300

    Criando a base já com a API

---
+++ src/script.js
@@ -0,0 +1,36 @@
+async function buscarClima(){
+
+  const cidade = document.getElementById("cidade").value
+
+  // BUSCAR COORDENADAS
+  const geoResponse = await fetch(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${cidade}&count=1&language=pt&format=json`
+  )
+
+  const geoData = await geoResponse.json()
+
+  const latitude = geoData.results[0].latitude
+  const longitude = geoData.results[0].longitude
+
+  // BUSCAR CLIMA
+  const climaResponse = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+  )
+
+  const climaData = await climaResponse.json()
+
+  // MOSTRAR NA TELA
+  document.getElementById("resultado").innerHTML = `
+  
+    <h2>${cidade}</h2>
+
+    <p>🌡 Temperatura: 
+      ${climaData.current_weather.temperature}°C
+    </p>
+
+    <p>💨 Vento:
+      ${climaData.current_weather.windspeed} km/h
+    </p>
+
+  `
+}
