e8f591b39b4bf087886db3ca3bccd1b94ad65cb7
Arthur-Calebe <arthurcarvalhodas@gmail.com>
2026-05-26 09:10:20 -0300

    adicionando o trim() para tirar os espaços finais e inicias do input

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function buscarClima(){
 
-  const cidade = document.getElementById("cidade").value
+  const cidade = document.getElementById("cidade").value.trim()
 
   // BUSCAR COORDENADAS
   const geoResponse = await fetch(
@@ -22,7 +22,7 @@ async function buscarClima(){
   // MOSTRAR NA TELA
   document.getElementById("resultado").innerHTML = `
   
-    <h2>${cidade}</h2>
+    <h2>${cidade}.trin</h2>
 
     <p>🌡 Temperatura: 
       ${climaData.current_weather.temperature}°C
